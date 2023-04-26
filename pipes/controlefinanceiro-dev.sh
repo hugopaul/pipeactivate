62b3102cbd3e43367f1720db9fdcc4136c315cd9
hugopaul <hugo.4500@icloud.com>
2023-04-25 22:23:32 -0300

    changes

--- pipes/controlefinanceiro-dev.sh
+++ pipes/controlefinanceiro-dev.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
+echo "######### entrando diretorio ###########"
 cd /opt/workspace/
+echo "######### done ###########"
+echo "######### removendo diretorio ###########"
 
 rm -rf controlefinanceiro
+echo "######### done ###########"
+echo "######### clonando projeto ###########"
 
 git clone https://github.com/hugopaul/controlefinanceiro.git
+echo "######### done ###########"
+echo "######### entrando diretorio clonado ###########"
 
 cd controlefinanceiro
+echo "######### done ###########"
+echo "######### trocando de branch  ###########"
 
 git checkout develop
+echo "######### done ###########"
+echo "######### fazendo pull ###########"
 
 git pull
+echo "######### done ###########"
+echo "######### buildando docker ###########"
 
 docker build -t=hugopaul/controlefinanceiro-dev -f Dockerfile-dev .
+echo "######### done ###########"
+echo "######### removendo imagem antiga ###########"
 
 docker rm controlefinanceiro-dev -f
+echo "######### done ###########"
+echo "######### rodando docker ###########"
 
 docker run -d -p 8081:8081 --name  controlefinanceiro-dev  hugopaul/controlefinanceiro-dev
+echo "######### done ###########"
+echo "######### removendo pasta ###########"
 
 rm -rf controlefinanceiro/
+echo "######### done ###########"
--- pipes/controlefinanceiro-prod.sh
+++ pipes/controlefinanceiro-prod.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
+echo "######### entrando diretorio ###########"
 cd /opt/workspace/
+echo "######### done ###########"
+echo "######### removendo diretorio ###########"
 
 rm -rf controlefinanceiro
+echo "######### done ###########"
+echo "######### clonando projeto ###########"
 
 git clone https://github.com/hugopaul/controlefinanceiro.git
+echo "######### done ###########"
+echo "######### entrando diretorio clonado ###########"
 
 cd controlefinanceiro
+echo "######### done ###########"
+echo "######### trocando de branch  ###########"
 
 git checkout prod
+echo "######### done ###########"
+echo "######### fazendo pull ###########"
 
 git pull
+echo "######### done ###########"
+echo "######### buildando docker ###########"
 
 docker build -t=hugopaul/controlefinanceiro-prod -f Dockerfile-prod .
+echo "######### done ###########"
+echo "######### removendo imagem antiga ###########"
 
 docker rm controlefinanceiro-prod -f
+echo "######### done ###########"
+echo "######### rodando docker ###########"
 
 docker run -d -p 8082:8082 --name  controlefinanceiro-prod  hugopaul/controlefinanceiro-prod
+echo "######### done ###########"
+echo "######### removendo pasta ###########"
 
 rm -rf controlefinanceiro/
+echo "######### done ###########"

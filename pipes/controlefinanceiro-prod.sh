0528558f845eef296e0b519e938e32f244197878
hugopaul <hugo.4500@icloud.com>
2023-05-01 14:45:34 -0300

    changes

--- pipes/controlefinanceiro-prod.sh
+++ pipes/controlefinanceiro-prod.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+sh /opt/workspace/pipeactivate/pipes/build-prod.sh &
+sleep 1m
+
 cd /opt/workspace/controlefinanceiro
 echo "######### done ###########"
 echo "######### removendo imagem antiga ###########"

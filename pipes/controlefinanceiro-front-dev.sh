6251a74520c97ebf67a01e171d799592dd17f0ad
hugopaul <hugo.4500@icloud.com>
2023-05-13 14:59:28 -0300

    changes

--- pipes/controlefinanceiro-front-dev.sh
+++ pipes/controlefinanceiro-front-dev.sh
@@ -1,49 +1,39 @@
 #!/bin/bash
 
 echo "######### entrando diretorio ###########"
-
 cd /opt/workspace/controlefinanceiro-front
 echo "######### done ###########"
 
 echo "#########  dando pull na branch ###########"
-
 git checkout dev
-
 git pull
-
 echo "######### done ###########"
+
 echo "#########  removendo pack lock ###########"
-
 rm -rf package-lock.json
-
 echo "######### done ###########"
+
 echo "#########  npm install ###########"
-
 npm install
-
 echo "######### done ###########"
+
 echo "######### removendo pasta dist ###########"
-
 rm -rf /opt/workspace/controlefinanceiro-front/dist
-
 echo "######### done ###########"
+
 echo "######### buildando projeto ###########"
-
 ng build --configuration=dev --base-href=/controle-dev/
-
-sleep 1m
+wait # aguarda ng build terminar
 echo "######### done ###########"
+
 echo "######### buildando docker ###########"
-
 docker build -f Dockerfile-dev  -t=hugopaul/controlefinanceiro-front-dev .
-
 echo "######### done ###########"
+
 echo "######### removendo imagem antiga ###########"
-
 docker rm "controlefinanceiro-front-dev" -f
-
 echo "######### done ###########"
-echo "######### rodando docker ###########"
 
+echo "######### rodando docker ###########"
 docker run -d -p 100:80 --name  controlefinanceiro-front-dev  hugopaul/controlefinanceiro-front-dev
 echo "######### done ###########"
--- pipes/controlefinanceiro-front-prod.sh
+++ pipes/controlefinanceiro-front-prod.sh
@@ -1,49 +1,39 @@
 #!/bin/bash
 
 echo "######### entrando diretorio ###########"
-
 cd /opt/workspace/controlefinanceiro-front
 echo "######### done ###########"
 
 echo "#########  dando pull na branch ###########"
-
 git checkout prod
-
 git pull
-
 echo "######### done ###########"
+
 echo "#########  removendo pack lock ###########"
-
 rm -rf package-lock.json
-
 echo "######### done ###########"
+
 echo "#########  npm install ###########"
-
 npm install
-
 echo "######### done ###########"
+
 echo "######### removendo pasta dist ###########"
-
 rm -rf /opt/workspace/controlefinanceiro-front/dist
-
 echo "######### done ###########"
+
 echo "######### buildando projeto ###########"
-
 ng build --configuration=prod --base-href=/controle/
-
-sleep 1m
+wait # aguarda ng build terminar
 echo "######### done ###########"
+
 echo "######### buildando docker ###########"
-
 docker build -f Dockerfile-prod  -t=hugopaul/controlefinanceiro-front-prod .
-
 echo "######### done ###########"
+
 echo "######### removendo imagem antiga ###########"
-
 docker rm "controlefinanceiro-front-prod" -f
-
 echo "######### done ###########"
-echo "######### rodando docker ###########"
 
+echo "######### rodando docker ###########"
 docker run -d -p 101:80 --name  controlefinanceiro-front-prod  hugopaul/controlefinanceiro-front-prod
 echo "######### done ###########"

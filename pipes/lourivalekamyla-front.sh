e1e8e24bab626e871c99fe7c48ebf9190438bcef
Hugo Paul <hugo.paul@hotmail.com>
2024-06-16 20:00:16 -0300

    adicionandoecommerce ppipe

--- pipes/lourivalekamyla-front.sh
+++ pipes/lourivalekamyla-front.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-echo "######### entrando diretorio ###########"
-cd /opt/workspace/lourivalekamyla-front
-echo "######### done ###########"
+set -e  # Exit immediately if a command exits with a non-zero status
 
-echo "#########  dando pull na branch ###########"
+# Defina suas variáveis
+WORKSPACE_DIR="/opt/workspace"
+REPO_NAME="lourivalekamyla-front"
+IMAGE_NAME="hugopaul/lourivalekamyla-front"
+CONTAINER_NAME="lourivalekamyla-front"
+
+echo "######### Entrando no diretório ###########"
+cd "$WORKSPACE_DIR/$REPO_NAME"
+echo "######### Done ###########"
+
+echo "######### Dando reset e pull na branch ###########"
 git reset --hard
-
 git checkout main
 git fetch
 git pull origin main --rebase
-echo "######### done ###########"
+echo "######### Done ###########"
 
-echo "######### buildando docker ###########"
-docker build -f Dockerfile  -t=hugopaul/lourivalekamyla-front .
-echo "######### done ###########"
+echo "######### Buildando Docker ###########"
+docker build -f Dockerfile -t="$IMAGE_NAME" .
+echo "######### Done ###########"
 
-echo "######### removendo imagem antiga ###########"
-docker rm "lourivalekamyla-front" -f
-echo "######### done ###########"
+echo "######### Removendo contêiner antigo ###########"
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    docker rm -f "$CONTAINER_NAME"
+fi
+echo "######### Done ###########"
 
-echo "######### rodando docker ###########"
-docker run -d -p 5000:80 --restart=always --name lourivalekamyla-front hugopaul/lourivalekamyla-front
-echo "######### done ###########"
+echo "######### Rodando Docker ###########"
+docker run -d -p 5000:80 --restart=always --name "$CONTAINER_NAME" "$IMAGE_NAME"
+echo "######### Done ###########"
